5ff5ef2f7bd67987a9b136df91a4aa304e557676
Balaji <balajimail@zoho.com>
2021-10-04 07:30:50 +0530

    Added Started

---
+++ 1_GettingStarted/FirstProgram.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+
+    int num;
+    std::cout<<"Enter Number: ";
+    std::cin>>num;
+    std::cout<<"The number entered is "<<num<<" my Fav number too"<<std::endl;
+    return 0;
+}
