0049e7660579fbf5d5581c3d86f834fe42019253
Balaji <balajimail@zoho.com>
2021-10-04 07:39:05 +0530

    2

---
+++ 2_NameSpaceVarients/varient2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int num;
+    cout<<"Enter Number: ";
+    cin>>num;
+    cout<<"The number entered is "<<num<<" my Fav number too"<<endl;
+    return 0;
+}
---
+++ 2_NameSpaceVarients/varient3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+int main(){
+
+    int num;
+    cout<<"Enter Number: ";
+    cin>>num;
+    cout<<"The number entered is "<<num<<" my Fav number too"<<endl;
+    return 0;
+}
